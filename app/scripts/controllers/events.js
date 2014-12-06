bba1aa25a2569566a8ca65514b3d1a8bed721dd6
Laurie Chan-Lam <lauriechanlam@gmail.com>
2014-12-06 12:37:50 +0000

    Displaying a dumb event

--- app/scripts/controllers/events.js
+++ app/scripts/controllers/events.js
@@ -2,16 +2,34 @@
 
 /**
  * @ngdoc function
- * @name londongdpaymentsystemApp.controller:AboutCtrl
+ * @name londongdpaymentsystemApp.controller:EventsCtrl
  * @description
- * # AboutCtrl
+ * # EventsCtrl
  * Controller of the londongdpaymentsystemApp
  */
 angular.module('londongdpaymentsystemApp')
-  .controller('EvetsCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('EventsCtrl', function($scope, $rootScope, eventsService) {
+    $scope.events = {
+        id: 0,
+        state: 1,
+        created_by: 4,
+        type: "training",
+        team: "Women 2nd Team",
+        date: "22/12/2015",
+        name: "Extra training in SportHouse",
+        description: "",
+        price: 5
+    };
+    /*$scope.getEventList = function() {  		  
+	  var promise = eventsService.getAllEvents();
+      promise.then(
+        function(events) { 
+          $scope.eventList = events;
+        },
+        function(errordata) {
+          console.log('Error', errordata);
+        }
+      );
+    };
+	$scope.getEventList();*/
   });
