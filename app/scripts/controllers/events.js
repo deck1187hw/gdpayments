fd3d9de4d8eb43b8b0040b508dbe735fea9096fd
Laurie Chan-Lam <lauriechanlam@gmail.com>
2014-12-06 13:09:00 +0000

    Having events being a table

--- app/scripts/controllers/events.js
+++ app/scripts/controllers/events.js
@@ -9,7 +9,8 @@
  */
 angular.module('londongdpaymentsystemApp')
   .controller('EventsCtrl', function($scope, $rootScope, eventsService) {
-    $scope.events = {
+    $scope.events = [
+    {
         id: 0,
         state: 1,
         created_by: 4,
@@ -19,7 +20,19 @@ angular.module('londongdpaymentsystemApp')
         name: "Extra training in SportHouse",
         description: "",
         price: 5
-    };
+    },
+    {
+        id: 1,
+        state: 2,
+        created_by: 6,
+        type: "match",
+        team: "Men 3rd Team",
+        date: "24/10/2014",
+        name: "Friendly against Stansted",
+        description: "",
+        price: 12
+    }
+    ];
     /*$scope.getEventList = function() {  		  
 	  var promise = eventsService.getAllEvents();
       promise.then(
---
+++ app/scripts/services/events.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name londongdpaymentsystemApp.events
+ * @description
+ * # events
+ * Service in the londongdpaymentsystemApp.
+ */
+
+
+
+
+angular.module('londongdpaymentsystemApp').factory('eventsService', function($http, $rootScope) {
+  var eventsService;
+  return eventsService;
+});
