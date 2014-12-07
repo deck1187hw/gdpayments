36b717e35ded96d243d33395a2a28f82855ce5a2
Laurie Chan-Lam <lauriechanlam@gmail.com>
2014-12-07 13:17:38 +0000

    Getting events from the database

--- app/scripts/controllers/events.js
+++ app/scripts/controllers/events.js
@@ -7,8 +7,18 @@
  * # EventsCtrl
  * Controller of the londongdpaymentsystemApp
  */
+
 angular.module('londongdpaymentsystemApp')
-  .controller('EventsCtrl', function($scope, $rootScope, eventsService) {
+    .controller('EventsCtrl', function ($scope, $http, eventsService) {
+  var promise = eventsService.getAllEvents();
+  promise.then(
+      function(response) { 
+        $scope.eventList = response;
+      },
+      function(errordata) {
+        console.log('Error', errordata);
+      }
+    );
     $scope.events = [
     {
         id: 0,
--- app/scripts/services/events.js
+++ app/scripts/services/events.js
@@ -8,10 +8,25 @@
  * Service in the londongdpaymentsystemApp.
  */
 
-
-
-
 angular.module('londongdpaymentsystemApp').factory('eventsService', function($http, $rootScope) {
-  var eventsService;
+  var eventsService = {
+  	getAllEvents: function () {
+	  var promise;
+      if (!promise) {
+      	var options = {
+          option: 'com_gdpayments',
+          task: 'get_events',
+          tmpl: 'component',
+          format: 'raw',
+          callback: 'JSON_CALLBACK'
+        };
+        var fullUrl = getUrlWithOptions($rootScope.siteUrl, options);
+        promise = $http.jsonp(fullUrl).then(function (response) {
+          return response.data;
+        });
+      }
+      return promise;
+    }
+  };
   return eventsService;
 });
