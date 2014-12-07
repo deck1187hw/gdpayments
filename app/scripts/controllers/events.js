84bbb1c767f35a353d5f4a062c37ff7d2d21bd9d
Laurie Chan-Lam <lauriechanlam@gmail.com>
2014-12-07 17:04:28 +0000

    Removing useless code

--- app/scripts/controllers/events.js
+++ app/scripts/controllers/events.js
@@ -9,9 +9,10 @@
  */
 
 angular.module('londongdpaymentsystemApp')
-    .controller('EventsCtrl', function ($scope, $http, eventsService) {
-  var promise = eventsService.getAllEvents();
-  promise.then(
+  .controller('EventsCtrl',
+  function ($scope, $http, eventsService) {
+    var promise = eventsService.getAllEvents();
+    promise.then(
       function(response) { 
         $scope.eventList = response;
       },
@@ -19,40 +20,5 @@ angular.module('londongdpaymentsystemApp')
         console.log('Error', errordata);
       }
     );
-    $scope.events = [
-    {
-        id: 0,
-        state: 1,
-        created_by: 4,
-        type: "training",
-        team: "Women 2nd Team",
-        date: "22/12/2015",
-        name: "Extra training in SportHouse",
-        description: "",
-        price: 5
-    },
-    {
-        id: 1,
-        state: 2,
-        created_by: 6,
-        type: "match",
-        team: "Men 3rd Team",
-        date: "24/10/2014",
-        name: "Friendly against Stansted",
-        description: "",
-        price: 12
-    }
-    ];
-    /*$scope.getEventList = function() {  		  
-	  var promise = eventsService.getAllEvents();
-      promise.then(
-        function(events) { 
-          $scope.eventList = events;
-        },
-        function(errordata) {
-          console.log('Error', errordata);
-        }
-      );
-    };
-	$scope.getEventList();*/
-  });
+  }
+);
