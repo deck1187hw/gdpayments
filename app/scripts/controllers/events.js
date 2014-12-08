679b8e7e8ff56821a7124831e01611d3fe7e3617
Laurie Chan-Lam <lauriechanlam@gmail.com>
2014-12-08 01:13:22 +0000

    Changed he way we get events (separated function)

--- app/scripts/controllers/events.js
+++ app/scripts/controllers/events.js
@@ -11,13 +11,18 @@
 angular.module('londongdpaymentsystemApp')
   .controller('EventsCtrl',
   function ($scope, $http, eventsService) {
-    var promise = eventsService.getAllEvents();
-    promise.then(
-      function(response) { 
-        $scope.eventList = response;
-      },
-      function(errordata) {
-        console.log('Error', errordata);
+    var getEventList = function() {
+      var promise = eventsService.getAllEvents();
+      promise.then(
+        function(response) { 
+          $scope.eventList = response;
+        },
+        function(errordata) {
+          console.log('Error', errordata);
+        }
+      );
+    };
+    getEventList();
       }
     );
   }
